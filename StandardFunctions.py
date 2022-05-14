d92d19e7c012b685a78c3ff19284ff1c47d6e0dc
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-05-14 12:43:33 +0530

    Standard Functions

--- StandardFunctions.py
+++ StandardFunctions.py
@@ -1,11 +1,38 @@
-def f(x, equ_list, deg):
+r'''
+    Python library for standard functions required for numerical methods.
+    - Method f()   - returns value of polynomial at given value.
+    - Mothod dof() - returns value of derivative of polynomial at given value.
+'''
+def f(x: int | float, equ_list: list, deg: int) -> int | float :
+    '''
+    Function for obtaining value of any polynomial at given value of x.
+    
+    Args : 
+        x : value of x
+        equ_list = list of coeffs of x in decreasing order.
+        deg : degree of polynomial.
+    
+    Returns: 
+        Returns float or int. 
+    '''
     power = deg
     sum = 0
     for coeff in equ_list:
         sum = sum + (coeff * (x**power))
         power = power - 1
     return sum
-def dof(x, equ_list, deg):
+def dof(x: int | float, equ_list: list, deg: int) -> int | float:
+    '''
+    Function for obtaining value of derivative of any polynomial at given value of x.
+    
+    Args : 
+        x : value of x
+        equ_list = list of coeffs of x in decreasing order.
+        deg : degree of polynomial.
+    
+    Returns: 
+        Returns float or int. 
+    '''
     power = deg
     diff = [0]*power
     i = 0
