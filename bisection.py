3c959cc8982712fce9c508fd9632cc993a67a892
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-05-14 12:43:18 +0530

    Numerical Methods

--- bisection.py
+++ bisection.py
@@ -1,19 +1,38 @@
-from StandardFunctions import f
+import StandardFunctions
 class BisectionMethod:
+    '''
+    Class for bisection Method.
+
+    Args : 
+        class constructor takes arguments.
+        equ_list : list of coeffs of x terms in polynomial.
+
+    Syntax :
+        obj = BisectionMethod(equ_list = mylist)
+    '''
     def __init__(self, equ_list) -> None:
         self.__equation = equ_list
         x0 = 0
         x1 = 1
         self.deg = len(equ_list) - 1
-        while((f(x0, self.__equation, self.deg)*f(x1, self.__equation, self.deg))>=0):
+        while((StandardFunctions.f(x0, self.__equation, self.deg)*StandardFunctions.f(x1, self.__equation, self.deg))>=0):
             x0 +=1
             x1 +=1
-        self.__fx1 = f(x0, self.__equation, self.deg)
-        self.__fx2 = f(x1, self.__equation, self.deg)
+        self.__fx1 = StandardFunctions.f(x0, self.__equation, self.deg)
+        self.__fx2 = StandardFunctions.f(x1, self.__equation, self.deg)
         self.__x0 = x0
         self.__x1 = x1
 
-    def bisectionMethod(self):
+    def bisectionMethod(self) -> int | float:
+        '''
+        Method for obtaining one root of given polynomial.
+
+        Args : 
+            none
+        
+        Returns : 
+            returns root as int or float.
+        '''
         fx1 = self.__fx1
         fx2 = self.__fx2
         x0 = self.__x0
@@ -26,7 +45,7 @@ class BisectionMethod:
             i = len(root_list)
             while(i>0):
                 prev_root = root_list[i-1]
-                if(f(prev_root, self.__equation, self.deg)*f(root, self.__equation, self.deg))<0:
+                if(StandardFunctions.f(prev_root, self.__equation, self.deg)*StandardFunctions.f(root, self.__equation, self.deg))<0:
                     root = (prev_root + root)/2
                     root_list.append(root)
                     break
--- newtonRaphson.py
+++ newtonRaphson.py
@@ -1,23 +1,43 @@
-from StandardFunctions import f, dof
+# from StandardFunctions import f, dof
+import StandardFunctions
 class NewtonRaphson:
+    '''
+    Class for Newton - Raphson Method.
+
+    Args : 
+        class constructor takes arguments.
+        equ_list : list of coeffs of x terms in polynomial.
+
+    Syntax :
+        obj = NewtonsRaphson(equ_list = mylist)
+    '''
     def __init__(self, equ_list) -> None:
         self.__equation = equ_list
         x0 = 0
         x1 = 1
         self.__deg = len(equ_list) - 1
-        while((f(x0, self.__equation, self.__deg)*f(x1, self.__equation, self.__deg))>=0):
+        while((StandardFunctions.f(x0, self.__equation, self.__deg)*StandardFunctions.f(x1, self.__equation, self.__deg))>=0):
             x0 +=1
             x1 +=1
-        self.__fx = f(x1, self.__equation, self.__deg)
-        self.__dfx = dof(x1, self.__equation, self.__deg)
+        self.__fx = StandardFunctions.f(x1, self.__equation, self.__deg)
+        self.__dfx = StandardFunctions.dof(x1, self.__equation, self.__deg)
         self.__x1 = x1
 
-    def newtonRaphsonMethod(self):
+    def newtonRaphsonMethod(self) -> int | float:
+        '''
+        Method for obtaining one root of given polynomial.
+
+        Args : 
+            none
+        
+        Returns : 
+            returns root as int or float.
+        '''
         x = self.__x1
         fx = self.__fx
         dfx = self.__dfx
         for i in range(10):
             x = x - (fx/dfx)
-            fx = f(x, self.__equation, self.__deg)
-            dfx = dof(x, self.__equation, self.__deg)
+            fx = StandardFunctions.f(x, self.__equation, self.__deg)
+            dfx = StandardFunctions.dof(x, self.__equation, self.__deg)
         return x
